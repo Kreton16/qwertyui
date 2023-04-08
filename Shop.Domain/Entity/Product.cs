47a9283347591bb24123b48324523665f81d4b71
Night0infinity <liubomyr.kurylo.ik.2022@lpnu.ua>
2023-04-08 22:22:26 +0300

    Add project files.

---
+++ Shop.Domain/Entity/Product.cs
@@ -0,0 +1,26 @@
+﻿
+using Shop.Domain.Enum;
+
+namespace Shop.Domain.Entity
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public CategoryAge CategoryAge { get; set; }
+        public CategoryGender CategoryGender { get; set; }
+        public CategorySeason CategorySeason { get; set; }
+        public CategorySize CategorySize { get; set; }
+        public CategoryTypes CategoryTypes { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public bool IsFeatured { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; } 
+    }
+}
---
+++ Shop.Domain/Enum/CategorySeason.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Shop.Domain.Enum
+{
+    public enum CategorySeason
+    {
+        [Display(Name = "Весна-Осінь")]
+        SpringAutumn = 0,
+        [Display(Name = "Зима")]
+        Winter = 1,
+        [Display(Name = "Літо")]
+        Summer = 2
+    }
+}
