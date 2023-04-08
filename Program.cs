b5933b9e6f7c7d58defe70c4d620bed81522246e
Kreton16 <renko0708@gmail.com>
2023-04-08 22:18:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2._4_OOP
+{
+    struct Dog
+    {
+        public string Name;
+        public string Mark;
+        public int Age;
+        public override string ToString()
+        {
+            return string.Format($"Собаку породи {Mark} звати {Name}, йому {Age} років.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog;
+            Console.Write("Enter dog's name: ");
+            myDog.Name = Console.ReadLine();
+            Console.Write("Enter dog's mark: ");
+            myDog.Mark = Console.ReadLine();
+            Console.Write("Enter dog's age: ");
+            myDog.Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write(myDog);
+
+            Console.ReadKey();
+        }
+    }
+}
